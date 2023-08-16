0e3a471bd9c4ba3fa5e4933e2180933ea77a4100
Lucas Batista <lucasbatista2408@gmail.com>
2023-08-16 15:33:11 -0300

    'done'

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class ContaTerminal {
+public class ContaTerminal {
+    int numero;
+    int agencia;
+    String nome;
+    double Saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        Saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        String response = "Olá "+nome+", obrigado por criar uma conta em nosso " +
+                "banco, sua agência é "+agencia+", conta "+numero+" e seu saldo " +
+                +Saldo+" já esta disponivel para saque.";
+        return response ;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        ContaTerminal c1 = new ContaTerminal();
+
+        System.out.println("Digite seu Nome:");
+        String nome = sc.nextLine();
+        c1.setNome(nome);
+
+        System.out.println("Digite sua Conta:");
+        int conta = sc.nextInt();
+        c1.setNumero(conta);
+
+        System.out.println("Digite sua Agencia:");
+        int agencia = sc.nextInt();
+        c1.setAgencia(agencia);
+
+        System.out.println("Digite seu Saldo:");
+        double saldo = sc.nextDouble();
+        c1.setSaldo(saldo);
+
+        System.out.println(c1.toString());
     }
 }
